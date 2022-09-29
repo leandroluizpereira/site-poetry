4bd6d76a121a96721e6993e1f966473da7c81f52
Leandro Luiz Pereira <45123616+leandroluizpereira@users.noreply.github.com>
2022-09-29 17:34:44 -0300

    Create function inictial

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -1,2 +1,14 @@
 
 alert("Poetry");
+
+var 
+var author-l = document.elementById("author-leandro");
+var author-v = document.elementaById("author-v");
+
+
+author-l.onclick = function (){
+
+     
+    
+
+}
