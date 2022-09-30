9086bd1fe8fe3d2e30cd15cbfd3aeab5e3e4a804
Leandro Luiz Pereira <45123616+leandroluizpereira@users.noreply.github.com>
2022-09-30 17:36:33 -0300

    Observation localstore and orientation object

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -3,12 +3,42 @@ alert("Poetry");
 
 var 
 var author-l = document.elementById("author-leandro");
-var author-v = document.elementaById("author-v");
+var author-v = document.elementById("author-v");
 
+//Revendo e tendo observações para o site 
+
+//Local store relembrando ações
+// Local store remember actions 
+// Em orientação objeto a 5 características 
+Polifornismo herança encapsulamento mutabilidade 
+// In orientation object the five characters
+// Da para usar na linguagem de programação javascript
+Em certo elementos como armazenamento utilizando do 
+// Próprio navegador o local store para pegar os dados
+// Getter para colocar os dados o setter bem utilizado
+// Em orientação ao objeto nas outras linguagens
+// Acho que e assim
+ // Localstore.getter ="";
+// Local store.setter ="";
+// Esta modalidade do local store  so tem um problema 
+// Que ele armazenado somente no navegador 
+// Se a pessoa deletar o cachê tudo que foi salvo 
+// Também será apagado 
+// Ele não e um armazenamento como banco de dados
+// Mas dá para quebrar alguns galhos para salvar 
+// Algo temporário ou para parar onde ultilizou últimas 
+// Vou utiliAr porque o github pages não aceita
+// Banco de dados ou php que utilizam servidores
+// Por ser uma página static estático 
+// Para fazer duas versões uma com local stores 
+// E outra com banco de dados My SQL 
 
 author-l.onclick = function (){
 
      
-    
+    author-l.style.background="#000000";
+    author-l.textContent="";
+    author-l.
 
+    
 }
