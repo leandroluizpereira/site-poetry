636f910440a62a8646eb1495af4dbe617279ceb0
leandroluizpereira <leandro.2306@hotmail.com>
2022-10-02 23:16:14 -0300

    Update function.js

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -1,44 +1,16 @@
 
 alert("Poetry");
 
-var author-l = document.elementById("author-leandro");
-var author-v = document.elementById("author-v");
+var authorL = document.elementById("author-leandro");
+var authorV = document.elementById("author-v");
 
-/*Revendo e tendo observações para o site 
 
-Local store relembrando ações
-Local store remember actions 
-Em orientação objeto a 5 características 
-Polifornismo herança encapsulamento mutabilidade 
- In orientation object the five characters
- Da para usar na linguagem de programação javascript
-Em certo elementos como armazenamento utilizando do 
- Próprio navegador o local store para pegar os dados
- Getter para colocar os dados o setter bem utilizado
-Em orientação ao objeto nas outras linguagens
-Acho que e assim
- Localstore.getter ="";
-Local store.setter ="";
-Esta modalidade do local store  so tem um problema 
-Que ele armazenado somente no navegador 
- Se a pessoa deletar o cachê tudo que foi salvo 
- Também será apagado 
- Ele não e um armazenamento como banco de dados
- Mas dá para quebrar alguns galhos para salvar 
- Algo temporário ou para parar onde ultilizou últimas 
- Vou utiliAr porque o github pages não aceita
-Banco de dados ou php que utilizam servidores
- Por ser uma página static estático 
- Para fazer duas versões uma com local stores 
- E outra com banco de dados My SQL 
-*/ 
-
-author-l.onclick = function (){
+authorL.onclick = function (){
 
      
-    author-l.style.background="#000000";
-    author-l.textContent="";
-    author-l.
+    authorL.style.background="#000000";
+    authorL.textContent="";
+    
 
     
 }
