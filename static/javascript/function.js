b8e73d8f259de34cdbd5e6d4480ac63f355ee21a
leandroluizpereira <leandro.2306@hotmail.com>
2022-10-05 01:44:22 -0300

    style

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -1,26 +1,44 @@
 
 alert("Poetry");
 
-var authorL = document.elementById("author-leandro");
-var authorV = document.elementById("author-vinicius");
-var photoPerfil = document.elementById("photo-perfil");
 
 
+var authorLe = document.getElementById("author-leandro");
+var authorVi = document.getElementById("author-vinicius");
+var photoPerfil = document.getElementById("photo-perfil");
+var titulo = document.getElementById("titulo");
+var subTitulo = document.getElementById("sub-titulo");
+var poetryOne = document.getElementById("poetry-one");
+var poetryTwo = document.getElementById("poetry-two");
+var poetryThree = document.getElementById("poetry-three");
+var poetryFour = document.getElementById("poetry-four");
+var poetryFive= document.getElementById("poetry-five");
 
 
+authorLe.onclick = function(){
+    photoPerfil.style.backgroundImage="url('static/imagem/leandro.jpg')";
+    alert("clicou leandro ok");
 
-authorL.onclick = function (){
-    console.log("leandro pereira "); 
-    alert("click leandro pereira ok");
-    authorL.style.background="#000000";
-    authorL.textContent="";
-    
 
     
 }
 
-authorV.onclick = function (){
-     alert("Click Vinius de moraes ok");
-     /*photoPerfil.style="background-image: url(../static/imagem/.jpg)";
-     */
+authorVi.onclick = function(){
+    alert("clicou vinicius ok");
+    photoPerfil.style.backgroundImage="url('static/imagem/th.jpg')";
+
+    titulo.textContent="Vinicius de Moraes";
+    titulo.style="color:yellow";
+    subTitulo.textContent="poesias";
+    subTitulo.style="color:yellow";
+
+    poetryOne.textContent="";
+    poetryTwo.textContent="";
+    poetryThree.textContent="";
+    poetryFour.textContent="";
+    poetryFive.textContent="";
+
+
 }
+
+
