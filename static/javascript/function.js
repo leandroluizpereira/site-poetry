b7cf8a5a5f5096c58fb6847614b32e031968a866
leandroluizpereira <leandro.2306@hotmail.com>
2022-10-04 23:26:26 -0300

    style

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -2,15 +2,25 @@
 alert("Poetry");
 
 var authorL = document.elementById("author-leandro");
-var authorV = document.elementById("author-v");
+var authorV = document.elementById("author-vinicius");
+var photoPerfil = document.elementById("photo-perfil");
+
+
+
 
 
 authorL.onclick = function (){
-
-     
+    console.log("leandro pereira "); 
+    alert("click leandro pereira ok");
     authorL.style.background="#000000";
     authorL.textContent="";
     
 
     
+}
+
+authorV.onclick = function (){
+     alert("Click Vinius de moraes ok");
+     /*photoPerfil.style="background-image: url(../static/imagem/.jpg)";
+     */
 }
